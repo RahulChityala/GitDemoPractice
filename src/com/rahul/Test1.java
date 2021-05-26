9c6707b3ef6be5438be08079d217df4274a0b9d3
RahulChityala <54503759+RahulChityala@users.noreply.github.com>
2021-05-26 14:27:13 +0530

    College details

---
+++ src/com/rahul/Test1.java
@@ -0,0 +1,17 @@
+package com.rahul;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String rollno = "1602-17-733-313";
+		String  name = "Chityala Rahul";
+		String branch = "CSE";
+		String company = "CITC";
+		System.out.println("Rollno:	"+rollno);
+		System.out.println("Name:	"+name);
+		System.out.println("Branch:	"+branch);
+		System.out.println("Company:"+company);
+	}
+
+}
